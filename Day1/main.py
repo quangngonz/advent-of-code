8c0c589168fdc3282f35aaccb80b74daa80125b5
quangngonz <46818455+quangngonz@users.noreply.github.com>
2023-12-06 07:31:49 +0000

    Day 1: Challenge

---
+++ Day1/main.py
@@ -0,0 +1,25 @@
+
+file_content = open('day1.txt', 'r')
+calibration_list = file_content.read().split('\n')
+
+total = 0
+
+def return_first_number(input_string):
+    for char in input_string:
+        if char.isdigit():
+            return char
+        
+def return_last_number(input_string):
+    new_string_list = input_string[::-1]
+    for char in new_string_list:
+        if char.isdigit():
+            return char
+
+for value in calibration_list:
+    first_number = return_first_number(value)
+    last_number = return_last_number(value)
+    calibration_value = int(first_number + last_number)
+
+    total += calibration_value
+
+print(total)    
