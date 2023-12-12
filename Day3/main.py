00897982bb2517ece95b74fbd69f0943cd7a13c1
quangngonz <46818455+quangngonz@users.noreply.github.com>
2023-12-12 06:25:46 +0000

    [Day 4] Part 1

--- Day3/main.py
+++ Day3/main.py
@@ -34,6 +34,16 @@ def get_surrounding_lines(line):
 
 def get_start_stop(index, value):
     if index == 0:
+        start_column, stop_collumn = 0,  len(str(value))+index
+    elif len(str(value))+index == 140:
+        start_column, stop_collumn = index, 140
+    else:
+        start_column, stop_collumn = index, len(str(value))+index
+    
+    return start_column, stop_collumn
+
+def get_start_stop_surroudings(index, value):
+    if index == 0:
         start_column, stop_collumn = 0,  len(str(value))+index+1
     elif len(str(value))+index == 140:
         start_column, stop_collumn = index-1, 140
@@ -47,7 +57,7 @@ def get_value_and_surroundings(value, index, line):
     surroundings_lines = get_surrounding_lines(line)
     # print("Value: {} Start: {} End: {} Line: {}".format(value, index, len(str(value))+index , line))
 
-    start_column, stop_collumn = get_start_stop(index, value)
+    start_column, stop_collumn = get_start_stop_surroudings(index, value)
     # print(start_column, stop_collumn)
 
     for line_index, sd_line in enumerate(surroundings_lines):
@@ -63,6 +73,14 @@ numbers_locations = []
 for line in input_text:
     asterisk_locations.append(find_all_asterisks(line))
     numbers_locations.append(find_all_numbers(line))
+
+for row, line in enumerate(numbers_locations):
+    print("Row",row, line)
+    for number, index in line:
+        start, stop = get_start_stop(index, number)
+        for column in range(start, stop):
+            data_table[row][column] = number
+        # print("Start: {} Stop: {} | {}".format(start, stop, data_table[row][start:stop]))
 
 for line_no, locations in enumerate(asterisk_locations):
     if locations != False:
