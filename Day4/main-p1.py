f99901e8827478ac6e65724186b9beb29dbaa49e
quangngonz <46818455+quangngonz@users.noreply.github.com>
2023-12-12 07:46:37 +0000

    [Day 4] Part 2 complete

---
+++ Day4/main-p1.py
@@ -0,0 +1,52 @@
+import re
+
+input_file = open('input.txt', 'r')
+input_text = input_file.read()
+input_text = input_text.split('\n')
+
+def split_numbers(input_string):
+    numbers = re.findall(r'\d+', input_string)  
+    numbers = [int(num) for num in numbers] 
+    return numbers
+
+def calculate_points(winning_nos):
+    points = 0
+    if len(winning_nos) == 0:
+        return 0
+    elif len(winning_nos) == 1:
+        return 1
+    else:
+        power = len(winning_nos) -1
+        return 2**power
+
+cards = []
+
+for card in input_text:
+    card_name, _ , card_content = card.partition(": ")
+    winning_numbers, _, numbers = card_content.partition(" | ")
+
+    winning_numbers = split_numbers(winning_numbers)
+    numbers = split_numbers(numbers)
+
+    cards.append([winning_numbers, numbers])
+
+total_points = 0
+
+for index, card in enumerate(cards):
+    points = 0
+    winning_numbers_card = []
+    winning_numbers, numbers = card[0], card[1]
+    
+    # print(winning_numbers, numbers)
+
+    for number in numbers:
+        if number in winning_numbers:
+            winning_numbers_card.append(number)
+    
+    points = calculate_points(winning_numbers_card)
+
+    print("Card: {} \tPoints: {} \tWinning:".format(index+1, points), end = " ")
+    print(*winning_numbers_card, sep = ", ")
+    total_points += points
+
+print("\t\tTotal: ", total_points)
--- Day4/main.py
+++ Day4/main.py
@@ -1,52 +1,57 @@
 import re
 
-input_file = open('input.txt', 'r')
-input_text = input_file.read()
-input_text = input_text.split('\n')
+def process_input_file(file_name):
+    input_file = open(file_name, 'r')
+    input_text = input_file.read()
+    input_text = input_text.split('\n')
+    input_file.close()
 
-def split_numbers(input_string):
-    numbers = re.findall(r'\d+', input_string)  
-    numbers = [int(num) for num in numbers] 
-    return numbers
+    def split_numbers(input_string):
+        numbers = re.findall(r'\d+', input_string)  
+        numbers = [int(num) for num in numbers] 
+        return numbers
 
-def calculate_points(winning_nos):
-    points = 0
-    if len(winning_nos) == 0:
-        return 0
-    elif len(winning_nos) == 1:
-        return 1
-    else:
-        power = len(winning_nos) -1
-        return 2**power
+    def print_winning_cards(index, winning_numbers_card):
+        print("Card: {} \tWinning:".format(index + 1), end=" ")
+        print(*winning_numbers_card, sep=", ")
 
-cards = []
+    cards = []
+    cards_winning_numbers = []
 
-for card in input_text:
-    card_name, _ , card_content = card.partition(": ")
-    winning_numbers, _, numbers = card_content.partition(" | ")
+    for card in input_text:
+        card_name, _ , card_content = card.partition(": ")
+        winning_numbers, _ , numbers = card_content.partition(" | ")
 
-    winning_numbers = split_numbers(winning_numbers)
-    numbers = split_numbers(numbers)
+        winning_numbers = split_numbers(winning_numbers)
+        numbers = split_numbers(numbers)
 
-    cards.append([winning_numbers, numbers])
+        cards.append([winning_numbers, numbers])
 
-total_points = 0
+    for index, card in enumerate(cards):
+        winning_numbers_card = []
+        winning_numbers, numbers = card[0], card[1]
 
-for index, card in enumerate(cards):
-    points = 0
-    winning_numbers_card = []
-    winning_numbers, numbers = card[0], card[1]
-    
-    # print(winning_numbers, numbers)
+        for number in numbers:
+            if number in winning_numbers:
+                winning_numbers_card.append(number)
 
-    for number in numbers:
-        if number in winning_numbers:
-            winning_numbers_card.append(number)
-    
-    points = calculate_points(winning_numbers_card)
+        cards_winning_numbers.append(winning_numbers_card)
+        # If you want to print, call the function here:
+        # print_winning_cards(index, winning_numbers_card)
 
-    print("Card: {} \tPoints: {} \tWinning:".format(index+1, points), end = " ")
-    print(*winning_numbers_card, sep = ", ")
-    total_points += points
+    return cards_winning_numbers
 
-print("\t\tTotal: ", total_points)
+winning_copies = [1] * 198
+
+file_name = 'input.txt'
+results = process_input_file(file_name)
+
+for card_no, result in enumerate(results):
+    copies_card = winning_copies[card_no]
+
+    if len(result) != 0:
+        for _ in range(copies_card):
+            for other_card in range(card_no+1, card_no+len(result)+1):
+                winning_copies[other_card] += 1
+
+print("Total number of scratchcards:", sum(winning_copies))
